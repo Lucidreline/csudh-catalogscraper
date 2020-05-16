614672ecbc8146cfbbbeb1531ae91fdfcdce4761
Lucidreline <techservs@pm.me>
2020-05-15 18:10:41 -0700

    returns a promise of all courses from the catelog in an array

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('https://whatsmygrade.org', (e, res, html) => {
-  if (!e && res.statusCode == 200) {
-    console.log(html);
-  } else {
-    console.log(e);
-  }
-});
+module.exports = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (e, res, html) => {
+      if (!e && res.statusCode == 200) {
+        const $ = cheerio.load(html);
+        const body = $('.programTables table');
+
+        let courseList = [];
+
+        body.each((i, table) => {
+          const tableRow = $(table).children('tbody').children('tr');
+          tableRow.each((i, row) => {
+            courseList.push({
+              name: $(row).find('.sc-coursenumber').text(),
+              number: $(row).find('.sc-coursetitle').text(),
+              credits: $(row).find('.credits').text(),
+            });
+          });
+        });
+        //console.log(courseList);
+        resolve(courseList);
+      } else {
+        reject(e);
+      }
+    });
+  });
+};
