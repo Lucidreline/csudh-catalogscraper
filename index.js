ad4795d2336a96e06a56d7fae561c30d8dd1a8cf
Lucidreline <techservs@pm.me>
2020-05-15 19:42:59 -0700

    switched to node fetch instead of requests

--- index.js
+++ index.js
@@ -1,35 +1,28 @@
-const request = require('request'); // lets us go to the url
+const fetch = require('node-fetch'); // lets us go to the url
 const cheerio = require('cheerio'); // lets us parse the html
 
-//
-
 module.exports = (_url) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     // we need a promise because gathering all of the catalog's data takes some time
     try {
-      request(_url, (error, respoce, html) => {
-        if (!error && respoce.statusCode == 200) {
-          const $ = cheerio.load(html); // creates a selector
-          const body = $('.programTables table'); // finds the div that holds all of the tables in the catalog
+      let res = await fetch(_url);
 
-          let courseList = []; // inits an array that will soon hold objects of courses
+      const $ = cheerio.load(await res.text()); // creates a selector for the page HTML
+      const body = $('.programTables table'); // finds the div that holds all of the tables in the catalog
 
-          body.each((i, table) => {
-            const tableRow = $(table).children('tbody').children('tr');
-            tableRow.each((i, row) => {
-              courseList.push({
-                // creates a course object and pushes it in the array
-                name: $(row).find('.sc-coursenumber').text(),
-                number: $(row).find('.sc-coursetitle').text(),
-                credits: $(row).find('.credits').text(),
-              });
-            });
+      let courseList = [];
+
+      body.each((i, table) => {
+        const tableRow = $(table).children('tbody').children('tr');
+        tableRow.each((i, row) => {
+          courseList.push({
+            name: $(row).find('.sc-coursenumber').text(),
+            number: $(row).find('.sc-coursetitle').text(),
+            credits: $(row).find('.credits').text(),
           });
-          resolve(courseList);
-        } else {
-          reject(e);
-        }
+        });
       });
+      resolve(courseList);
     } catch (err) {
       reject(err);
     }
