2af1394db548154c082a7540483f1bd02ccbe6f0
Lucidreline <techservs@pm.me>
2020-05-22 19:31:56 -0700

    added a mode to the fetch

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const fetch = require('node-fetch'); // lets us go to the url
 const cheerio = require('cheerio'); // lets us parse the html
 
-module.exports = (_url) => {
+module.exports = _url => {
   return new Promise(async (resolve, reject) => {
     // we need a promise because gathering all of the catalog's data takes some time
     try {
-      let res = await fetch(_url);
+      let res = await fetch(_url, {
+        mode: 'no-cors',
+      });
 
       const $ = cheerio.load(await res.text()); // creates a selector for the page HTML
       const body = $('.programTables table'); // finds the div that holds all of the tables in the catalog
